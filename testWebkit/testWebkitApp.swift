98000a85d3d792387a472027c21a8699fced3da3
Sum Bui <buingocsum@gmail.com>
2024-02-01 15:05:39 +0700

    Initial Commit

---
+++ testWebkit/testWebkitApp.swift
@@ -0,0 +1,17 @@
+//
+//  testWebkitApp.swift
+//  testWebkit
+//
+//  Created by dr.Strange on 01/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct testWebkitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
